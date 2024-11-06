680973ef5e7c838f5a899adc18d8a85cd2eee7ef
MacDuki <PCMYACT@hotmail.com>
2024-11-05 22:52:50 -0300

    Comienzo pruebas MVC

---
+++ C#/M-V-C/testing Front/testing Front/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testing_Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
